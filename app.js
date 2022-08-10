3ef105cb51faa87524301a64ebfb23e0fcc90310
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-08-10 07:24:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+var items=["milk"];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));  
 app.get("/",function(req,res){
-res.send("Hello");
+
+
+    var today = new Date();
+    
+    var options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+        
+    res.render("list", {kindOfDay: day, newListItems: items});
 });
 
-var today = new Date();
-var day = today.getDay();
-console.log(day);
+
+app.post("/",function(req,res){
+ var item = req.body.newItem;
+ items.push(item); 
+ res.redirect("/");
+});
+
 
 app.listen(3000,function(){
     console.log("server started at port 3000");
